8eee5260ad58e4b03000f7f8723e7054bc8c63b0
Abhishek26096 <141644939+Abhishek26096@users.noreply.github.com>
2025-01-03 13:32:13 +0530

    Update 02_array.js

--- 02_array.js
+++ 02_array.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Abhishek"))
+console.log(Array.from("Abhishek"))
+console.log(Array.from({name: "Abhishek"})) // interesting
 
 let score1 = 100
 let score2 = 200
